66b17ec16ecae49fe1a811e2019f03f66be3c49d
Pranjal Shukla <pranjal779@gmail.com>
2023-02-15 17:18:48 -0500

    (#49)

--- pythonNotes/JavaScript String Methods.js
+++ pythonNotes/JavaScript String Methods.js
@@ -6,3 +6,19 @@ function func() {
 }
 func();
 
+-----
+  
+let gfg = 'GeeksforGeeks';
+let geeks = 'GfG is the best platform to learn and\n'+
+'experience Computer Science.';
+
+// Print the string as it is
+console.log(gfg);
+
+console.log(geeks);
+
+// As string index starts from zero
+// It will return first character of string
+console.log(gfg.charAt(0));
+
+console.log(geeks.charAt(5));
