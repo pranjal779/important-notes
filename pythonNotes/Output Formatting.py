f06f4d394a2afb7ae94a9ac3637374692fde1435
Pranjal Shukla <pranjal779@gmail.com>
2023-02-15 12:49:17 -0500

    (#39).....

--- pythonNotes/Output Formatting.py
+++ pythonNotes/Output Formatting.py
@@ -19,3 +19,31 @@ print("Second argument: {1:3d}, first one: {0:7.2f}".
  
 print("Geeks: {a:5d},  Portal: {p:8.2f}".
      format(a = 453, p = 59.058))
+
+
+
+
+----------
+
+
+
+# Python program to
+# show format() is
+# used in dictionary
+ 
+tab = {'geeks': 4127, 'for': 4098, 'geek': 8637678}
+ 
+# using format() in dictionary
+print('Geeks: {0[geeks]:d}; For: {0[for]:d}; '
+    'Geeks: {0[geek]:d}'.format(tab))
+    
+# using format() in dictionary instead of 0 in  {0[for]:d}?
+"""Traceback (most recent call last):
+  File "main.py", line 12, in <module>
+    print('Geeks: {0[geeks]:d}; For: {1[for]:d}; '
+IndexError: Replacement index 1 out of range for positional args tuple"""
+ 
+data = dict(fun ="GeeksForGeeks", adj ="Portal")
+ 
+# using format() in dictionary
+print("I love {fun} computer {adj}".format(**data))
