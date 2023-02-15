9bb956a0eeb61900d6137a8b34f4eb077248cf8c
Pranjal Shukla <pranjal779@gmail.com>
2023-02-15 17:06:49 -0500

    (#45)

---
+++ pythonNotes/Formatting of Strings.py
@@ -0,0 +1,16 @@
+# Integers such as Binary, hexadecimal, etc., and floats can be rounded or displayed in the exponent form with the use of format specifiers. 
+
+# Formatting of Integers
+String1 = "{0:b}".format(16)
+print("\nBinary representation of 16 is ")
+print(String1)
+
+# Formatting of Floats
+String1 = "{0:e}".format(165.6458)
+print("\nExponent representation of 165.6458 is ")
+print(String1)
+
+# Rounding off Integers
+String1 = "{0:.2f}".format(1/6)
+print("\none-sixth is : ")
+print(String1)
