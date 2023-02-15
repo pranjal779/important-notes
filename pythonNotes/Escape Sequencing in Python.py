2c9a84bb7c0bf2c5ba9bd1dae6a5c8257d5f7dff
Pranjal Shukla <pranjal779@gmail.com>
2023-02-15 17:05:13 -0500

    (#44)

--- pythonNotes/Escape Sequencing in Python.py
+++ pythonNotes/Escape Sequencing in Python.py
@@ -34,3 +34,30 @@ print(String1)
 String1 = "Python\nGeeks"
 print("\nNew Line: ")
 print(String1)
+
+
+-----
+
+# To ignore the escape sequences in a String, r or R is used, this implies that the string is a raw string and escape sequences inside it are to be ignored.
+
+# Printing hello in octal
+String1 = "\110\145\154\154\157"
+print("\nPrinting in Octal with the use of Escape Sequences: ")
+print(String1)
+
+# Using raw String to
+# ignore Escape Sequences
+String1 = r"This is \110\145\154\154\157"
+print("\nPrinting Raw String in Octal Format: ")
+print(String1)
+
+# Printing Geeks in HEX
+String1 = "This is \x47\x65\x65\x6b\x73 in \x48\x45\x58"
+print("\nPrinting in HEX with the use of Escape Sequences: ")
+print(String1)
+
+# Using raw String to
+# ignore Escape Sequences
+String1 = r"This is \x47\x65\x65\x6b\x73 in \x48\x45\x58"
+print("\nPrinting Raw String in HEX Format: ")
+print(String1)
