771338f7204ac92db84ff7b14ce75236b891dd60
Pranjal Shukla <pranjal779@gmail.com>
2023-02-15 14:29:17 -0500

    (#41)

--- pythonNotes/Output Formatting.py
+++ pythonNotes/Output Formatting.py
@@ -72,3 +72,29 @@ print (cstr.ljust(40, '-'))
 # with "-" padding 
 print ("The right aligned string is : ")
 print (cstr.rjust(40, '-'))
+
+
+-----
+
+# Python program to demonstrate ternary operator
+a, b = 10, 20
+  
+# Use tuple for selecting an item
+# (if_test_false,if_test_true)[test]
+# if [a<b] is true it return 1, so element with 1 index will print
+# else if [a<b] is false it return 0, so element with 0 index will print
+print( (b, a) [a < b] )
+  
+# Use Dictionary for selecting an item
+# if [a < b] is true then value of True key will print
+# else if [a<b] is false then value of False key will print 
+print({True: a, False: b} [a < b])
+  
+# lambda is more efficient than above two methods
+# because in lambda  we are assure that
+# only one expression will be evaluated unlike in
+# tuple and Dictionary
+print((lambda: b, lambda: a)[a < b]())
+
+
+----
