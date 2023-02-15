a875d16d798efce778e3bf50f9279fb093ae385f
Pranjal Shukla <pranjal779@gmail.com>
2023-02-15 17:03:14 -0500

    (#43)

---
+++ pythonNotes/Escape Sequencing in Python.py
@@ -0,0 +1,36 @@
+# Python Program for
+# Escape Sequencing
+# of String
+
+# Initial String
+String1 = '''I'm a "Geek"'''
+print("Initial String with use of Triple Quotes: ")
+print(String1)
+
+# Escaping Single Quote
+String1 = 'I\'m a "Geek"'
+print("\nEscaping Single Quote: ")
+print(String1)
+
+# Escaping Double Quotes
+String1 = "I'm a \"Geek\""
+print("\nEscaping Double Quotes: ")
+print(String1)
+
+# Printing Paths with the
+# use of Escape Sequences
+String1 = "C:\\Python\\Geeks\\"
+print("\nEscaping Backslashes: ")
+print(String1)
+
+# Printing Paths with the
+# use of Tab
+String1 = "Hi\tGeeks"
+print("\nTab: ")
+print(String1)
+
+# Printing Paths with the
+# use of New Line
+String1 = "Python\nGeeks"
+print("\nNew Line: ")
+print(String1)
