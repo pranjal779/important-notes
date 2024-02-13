c8de28f2dd51662bbbeb6743314f359b2de0ffd3
Pranjal Shukla <pranjal779@gmail.com>
2024-02-13 09:40:13 -0500

    Git commands docs

---
+++ Personnel notes/commands.sh
@@ -0,0 +1,12 @@
+git help git
+
+# -----------
+# git-clean - Remove untracked files from the working tree
+
+# # file:///C:/Program%20Files/Git/mingw64/share/doc/git-doc/git-clean.html
+# SYNOPSIS
+# git clean [-d] [-f] [-i] [-n] [-q] [-e <pattern>] [-x | -X] [--] [<pathspec>…​]
+
+# --------------
+
+# file:///C:/Program%20Files/Git/mingw64/share/doc/git-doc/git.html
