435ebe6a823e5b2d1ef027a881942662b0490d88
Pranjal Shukla <pranjal779@gmail.com>
2024-02-22 19:50:28 -0500

    Create list_Comprehension.py

---
+++ pythonNotes/essentialsPY101/list_Comprehension.py
@@ -0,0 +1,16 @@
+x = [i for i in range(10)]
+
+print(x)
+
+x2 = [[] for i in range(10)]
+
+print(x2)
+
+x3 = [[j for j in range(5)] for i in range(10)]
+
+print(x3)
+
+x4 = [i for i in range(10) if i % 2 == 0]
+
+print(x4)
+
