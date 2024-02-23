59b097658e605fa6f95ce53c2e38c54ec94041a6
Pranjal Shukla <pranjal779@gmail.com>
2024-02-22 19:38:54 -0500

    Create sample01.py

---
+++ pythonNotes/essentialsPY101/sample01.py
@@ -0,0 +1,11 @@
+def get_largest_numbers(numbers, n):
+  numbers.sort()
+
+  return numbers[-n:]
+
+
+nums = [2, 3, 4, 1, 34, 123, 231, 1]
+
+print(nums)
+largest = get_largests(nums, 2)
+print(nums)
