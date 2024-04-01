501c4abee0f68176f2c5cbb3d3193696922c2c38
Pranjal Shukla <pranjal779@gmail.com>
2024-04-01 19:59:55 -0400

    Update commands.sh

--- Personnel notes/commands.sh
+++ Personnel notes/commands.sh
@@ -1,3 +1,12 @@
+# To untrack the files that were added with git add -A but you don't want to delete them from your local directory, you can use the git reset command to unstage them. Here's what you can do:
+
+```bash
+git reset
+```
+This command will unstage all changes that have been added to the index, effectively removing them from the staging area.
+
+
+
 git help git
 
 # -----------
