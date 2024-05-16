cb9b9e48164a00f9b48af23a5d63f8b2252c88e8
Pranjal Shukla <pranjal779@gmail.com>
2024-05-16 09:08:40 -0400

    Section 2

---
+++ terraForm/myLearnings/Section 2/demo.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "name0912" {
+  ami           = "ami-............."
+  instance_type = "t2.micro"
+}
+
+resource "aws_instance" "myec2-Demo" {
+  ami           = "ami-00c39f71452c08778"
+  instance_type = "t2.micro"
+}
