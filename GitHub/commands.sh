b549e84513fa6174d1cac1d2e36bd675ddf85118
Pranjal Shukla <pranjal779@gmail.com>
2025-11-10 13:23:17 +0530

    commands refresher

---
+++ GitHub/commands.sh
@@ -0,0 +1,31 @@
+git init
+
+git pull
+
+git pull origin main
+
+ls -a
+
+ls
+
+ls -ltra
+
+git status
+
+git log
+
+git log --online
+
+git rm --cached <file/pathTofile> #unstage/stage 
+
+git diff
+
+git add .
+
+git checkout
+
+gh pr create --file "The bug is fixed" --body "Everything works again"
+
+#shebang files
+# #!/bin/badges
+
